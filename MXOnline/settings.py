fe8762776d4747febb2eddd05f4740b8cfe43d4b
yynn-123 <55397241+yynn-123@users.noreply.github.com>
2020-04-27 23:26:01 +0800

    账户信息

--- MXOnline/settings.py
+++ MXOnline/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users.apps.UsersConfig',
+    'apps.courses.apps.CoursesConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,7 +120,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-
+AUTH_USER_MODEL = "users.UserProfile"
 
 
 # Static files (CSS, JavaScript, Images)
