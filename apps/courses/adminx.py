bd52845676ef0adb0396b8dd6580f5381ea28fb9
yynn-123 <55397241+yynn-123@users.noreply.github.com>
2020-04-28 20:23:24 +0800

    课程信息栏目

---
+++ apps/courses/adminx.py
@@ -0,0 +1,10 @@
+import xadmin
+from apps.courses.models import Course
+#不用继承admin.ModelAdmin
+class CourseAdmin(object):
+    list_display = ['id','name','desc','learn_times','degree']
+    search_fields = ['name']
+    list_filter = ['id','name','desc','learn_times','degree']
+
+
+xadmin.site.register(Course,CourseAdmin)
